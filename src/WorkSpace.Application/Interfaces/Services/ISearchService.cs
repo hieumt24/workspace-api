c61c6f092927ca434df4e2c55a625e0e1a20ffd7
hieu.maitrong <hieu.maitrong@nashtechglobal.com>
2025-12-04 21:22:05 +0700

    merge to main

---
+++ src/WorkSpace.Application/Interfaces/Services/ISearchService.cs
@@ -0,0 +1,15 @@
+﻿using WorkSpace.Application.DTOs.WorkSpaces;
+using WorkSpace.Application.Wrappers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WorkSpace.Application.Interfaces.Services
+{
+    public interface ISearchService
+    {
+        Task<Response<IEnumerable<WorkSpaceSearchResultDto>>> SearchWorkSpacesAsync(SearchRequestDto request);
+        Task<IEnumerable<string>> GetLocationSuggestionsAsync(string query);
+        Task<IEnumerable<string>> GetAllWardsAsync();
+        Task<Response<IEnumerable<RoomWithAmenitiesDto>>> SearchRoomsInWorkSpaceAsync(int workSpaceId, SearchRoomsInWorkSpaceRequestDto request);
+    }
+}
